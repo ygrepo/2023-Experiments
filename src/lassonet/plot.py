c71026964456f43de8287f18cd01349fd4eb5f5c
Yves Greatti <yvgrotti@gmail.com>
2023-01-16 16:49:42 -0500

    Added COX model

---
+++ src/lassonet/plot.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+
+from .utils import eval_on_path
+
+
+def plot_path(model, path, X_test, y_test, *, score_function=None):
+    """
+    Plot the evolution of the model on the path, namely:
+    - lambda
+    - number of selected variables
+    - score
+
+
+    Parameters
+    ==========
+    model : LassoNetClassifier or LassoNetRegressor
+    path
+        output of model.path
+    X_test : array-like
+    y_test : array-like
+    score_function : function or None
+        if None, use score_function=model.score
+        score_function must take as input X_test, y_test
+    """
+    # TODO: plot with manually computed score
+    score = eval_on_path(model, path, X_test, y_test, score_function=score_function)
+    n_selected = [save.selected.sum() for save in path]
+    lambda_ = [save.lambda_ for save in path]
+
+    plt.figure(figsize=(16, 16))
+
+    plt.subplot(311)
+    plt.grid(True)
+    plt.plot(n_selected, score, ".-")
+    plt.xlabel("number of selected features")
+    plt.ylabel("score")
+
+    plt.subplot(312)
+    plt.grid(True)
+    plt.plot(lambda_, score, ".-")
+    plt.xlabel("lambda")
+    plt.xscale("log")
+    plt.ylabel("score")
+
+    plt.subplot(313)
+    plt.grid(True)
+    plt.plot(lambda_, n_selected, ".-")
+    plt.xlabel("lambda")
+    plt.xscale("log")
+    plt.ylabel("number of selected features")
+
+    plt.tight_layout()
